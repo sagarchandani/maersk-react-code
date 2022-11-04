77371ead9e5affbb46f6956b8eebeea6af68b26b
Sagar Chandani <ssshackathon@gmail.com>
2022-11-04 21:16:14 +0000

    task(): maersk react coding exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [matrixArray, setMatrixArray] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const shuffleMatrix = (e) => {
+    e.preventDefault();
+    let newArray = [...matrixArray];
+    for (let i = matrixArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = newArray[i];
+      newArray[i] = newArray[j];
+      newArray[j] = temp;
+    }
+    setMatrixArray(newArray);
+  };
+  const sortMatrix = (e) => {
+    e.preventDefault();
+    let newArray = [...matrixArray];
+    newArray.sort();
+    setMatrixArray(newArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="header">Shuffle and Sort</header>
+      <div className="container">
+        <div className="cardMatrix" id="cardMatrix">
+          {matrixArray.map((element, index) => (
+            <div className="card" key={index}>
+              {element}
+            </div>
+          ))}
+        </div>
+        <div className="buttonDiv">
+          <button
+            onClick={(e) => shuffleMatrix(e)}
+            className="button"
+          >
+            Shuffle
+          </button>
+          <button onClick={(e) => sortMatrix(e)} className="button">
+            Sort
+          </button>
+        </div>
+      </div>
+      <footer>Shuffle and sort by Sagar Chandani</footer>
     </div>
   );
 }
